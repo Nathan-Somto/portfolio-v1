7887d2b881b03b5b24ddb38a924d45b18d43397c
Mkparu Somtochi <92867046+Nathan-Somto@users.noreply.github.com>
2023-09-26 14:14:11 +0100

    Update index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from "react";
 /* import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu"; */
